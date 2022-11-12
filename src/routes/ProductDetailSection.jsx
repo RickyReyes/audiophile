1961a7d7be4d035d6303c119b349ce14387ce5f8
RickyReyes <77644172+RickyReyes@users.noreply.github.com>
2022-11-12 02:18:01 -0800

    detail section progress

--- src/routes/ProductDetailSection.jsx
+++ src/routes/ProductDetailSection.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 const ProductDetailSection = ({ product }) => {
   const navigate = useNavigate();
+  console.log(product);
   return (
     <section className="product-detail">
       <p className="go-back" onClick={() => navigate(-1)}>
@@ -15,16 +16,47 @@ const ProductDetailSection = ({ product }) => {
       />
       {product.new && <small>New Product</small>}
       <h3>{product.name}</h3>
-      <p className="product-detail__desc">{product.description}</p>
+      <p>{product.description}</p>
       <strong>$ {product.price.toLocaleString("en-US")}</strong>
       <div className="amt-and-btn-flex">
         <div className="amt-flex">
-          <p>-</p>
-          <p>1</p>
-          <p>+</p>
+          <span>-</span>
+          <span>1</span>
+          <span>+</span>
         </div>
         <button>Add to Cart</button>
       </div>
+      <h4>Features</h4>
+      <p>{product.features}</p>
+      <h4>In the box</h4>
+      <ul className="in-the-box">
+        {product.includes.map(({ quantity, item }) => (
+          <li>
+            <span>{quantity}x</span>
+            <p>{item}</p>
+          </li>
+        ))}
+      </ul>
+      <ul className="">
+        <li>
+          <img
+            src={product.gallery.first.mobile.substring(1)}
+            alt={`${product.name} showcase`}
+          />
+        </li>
+        <li>
+          <img
+            src={product.gallery.second.mobile.substring(1)}
+            alt={`${product.name} showcase`}
+          />
+        </li>
+        <li>
+          <img
+            src={product.gallery.third.mobile.substring(1)}
+            alt={`${product.name} showcase`}
+          />
+        </li>
+      </ul>
     </section>
   );
 };
