4c929c90ecd6dad52bf84695691a3980c6096c4f
RickyReyes <77644172+RickyReyes@users.noreply.github.com>
2023-01-16 15:46:55 -0800

    cart summary progress

--- src/components/CheckoutSummary.jsx
+++ src/components/CheckoutSummary.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CartContext } from "../cartContext";
 
 const CheckoutSummary = () => {
-  return <section className="checkout__summary">Summary</section>;
+  const { cart } = useContext(CartContext);
+  return (
+    <section className="checkout__summary">
+      <h2 className="checkout__summary__heading">Summary</h2>
+      <ul className="checkout__summary__cart-ul">
+        {cart.map((cartItem) => {
+          return (
+            <li className="checkout__cart__row">
+              <img
+                className="checkout__cart__img"
+                src={cartItem.product.image.mobile.substring(1)}
+                alt={cartItem.product.name}
+              />
+              <div className="checkout__cart__name-price-flex">
+                <div className="price">{cartItem.product.name}</div>
+                <div className="checkout__cart__name">
+                  ${cartItem.product.price}
+                </div>
+              </div>
+              <div className="checkout__cart__quantity">
+                x{cartItem.quantity}
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
 };
 
 export default CheckoutSummary;
